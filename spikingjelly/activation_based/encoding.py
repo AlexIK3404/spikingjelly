164783f765b53459dcd7bcb630d46a6a1969b2dc
Александр Коняев <121352164+AlexIK3404@users.noreply.github.com>
2026-01-11 14:17:39 +0300

    Add files via upload

--- spikingjelly/activation_based/encoding.py
+++ spikingjelly/activation_based/encoding.py
@@ -302,8 +302,35 @@ class LatencyEncoder(StatefulEncoder):
         self.spike = self.spike.permute(d_seq)
 
 
-import torch
-import torch.nn as nn
+class PoissonEncoder(StatelessEncoder):
+    def __init__(self, step_mode='s'):
+        """
+        * :ref:`API in English <PoissonEncoder.__init__-en>`
+
+        .. _PoissonEncoder.__init__-cn:
+
+        无状态的泊松编码器。输出脉冲的发放概率与输入 ``x`` 相同。
+
+        .. warning::
+
+            必须确保 ``0 <= x <= 1``。
+
+        * :ref:`中文API <PoissonEncoder.__init__-cn>`
+
+        .. _PoissonEncoder.__init__-en:
+
+        The poisson encoder will output spike whose firing probability is ``x``。
+
+        .. admonition:: Warning
+            :class: warning
+
+            The user must assert ``0 <= x <= 1``.
+        """
+        super().__init__(step_mode)
+
+    def forward(self, x: torch.Tensor):
+        out_spike = torch.rand_like(x).le(x).to(x)
+        return out_spike
 
 class PoissonEncoderWithDt(nn.Module):
     """
